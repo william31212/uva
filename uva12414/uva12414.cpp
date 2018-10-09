1509ddc7a26e7402049684c1800a6678b9038518
william31212 <william31212@gmail.com>
2018-10-09 09:15:55 +0800

    uva12414

---
+++ uva12414/uva12414.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+
+	#ifndef ONLINE_JUDGE
+	freopen("uva12414.in","r",stdin);
+	freopen("uva12414.out","w",stdout);
+	#endif
+
+	cout << "fuck" << endl;
+
+	return 0;
+}
+	
