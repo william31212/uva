7ebfcbe476cdbc4ca735442e9f09b1ecc0ca1e49
halloworld <william31212@gmail.com>
2019-10-20 17:32:43 +0800

    [uva11367]

--- uva11367/uva11367.cpp
+++ uva11367/uva11367.cpp
@@ -4,149 +4,133 @@ using namespace std;
 
 #define TESTC ""
 #define PROBLEM "uva11367"
-#define SIZEM 1005
-#define INF 1e9
+#define MAXN 1005
+#define PB push_back
+#define P pair<int, int>
+#define F first
+#define S second
 
-#define USE_CPPIO() ios_base::sync_with_stdio(0); cin.tie(0)
+#define USE_CPPIO()               \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(0)
 
-
-struct Edge
+struct State
 {
-	int from,to,dis;
-};
 
-struct node
-{
-	int point,oil,cost;//要到那個點，剩餘油量，已花費
-	bool operator < (const node &input) const
+	int fr, oil, cost;
+	friend bool operator<(const State &lhs, const State &rhs)
 	{
-		return cost > input.cost;//價格少的為優先
+		return lhs.cost > rhs.cost;
 	}
 };
-int city,road;
-vector <Edge> edges;
-vector <int> G[SIZEM];
-bool visited[SIZEM][SIZEM];
-int oil_price[SIZEM];
-int dp[SIZEM][SIZEM];
 
-void Addedge(int from,int to,int dis)
+int city, road, p[MAXN], q, c, s, e;
+int vis[MAXN][MAXN], dp[MAXN][105];
+vector<P> G[MAXN];
+
+void debug()
 {
-	edges.push_back((Edge){from,to,dis});
-	edges.push_back((Edge){to,from,dis});
-	int m = edges.size();
-
-	G[from].push_back(m-2);
-	G[to].push_back(m-1);
+	for (int i = 0; i < city; i++)
+	{
+		for (int j = 0; j < c; j++)
+			printf("%d ", dp[i][j]);
+		printf("\n");
+	}
+	printf("------------------------\n");
 }
 
-int run_dijkstra(int oil_size,int start,int end)
+void add_edge(int fr, int to, int len)
 {
-	priority_queue <node> que;
-	// memset(dp,INF,sizeof(dp));
-	for (int i = 0; i < SIZEM; ++i)
+	G[fr].PB({to, len});
+	G[to].PB({fr, len});
+}
+
+int dijk(int s, int e, int c)
+{
+	memset(vis, 0, sizeof(vis));
+
+	for (int i = 0; i < city; i++)
+		for (int j = 0; j <= c; j++)
+			dp[i][j] = INT_MAX;
+	priority_queue<State> pq;
+
+	//to, oil, cost
+	dp[s][0] = 0;
+	pq.push({s, 0, dp[s][0]});
+
+	while (!pq.empty())
 	{
-		for (int j = 0; j < SIZEM; ++j)
-		{
-			dp[i][j] = INF;
-		}
-	}
-	memset(visited,0,sizeof(visited));
-	dp[start][0] = 0;
+		State cur = pq.top();
+		pq.pop();
 
-	que.push((node){start,0,dp[start][0]});
-
-
-	while(!que.empty())
-	{
-		node cur = que.top();
-		que.pop();
-
-		int top_point = cur.point;
-		int top_oil = cur.oil;
-
-		if (visited[top_point][top_oil])
+		if (vis[cur.fr][cur.oil])
 			continue;
 
-		visited[top_point][top_oil] = true;
+		vis[cur.fr][cur.oil] = 1;
 
-		for (int i = 0; i < G[top_point].size(); ++i)
+		for (auto i : G[cur.fr])
 		{
-			int too = edges[ G[top_point][i] ].to;
-			int d = edges[ G[top_point][i] ].dis;
-			
-			if (d > top_oil)//油不夠
+			int to = i.F;
+			int road_len = i.S;
+
+			//油不夠
+			if (cur.oil < road_len)
 				continue;
-			if (dp[too][top_oil - d] > dp[top_point][top_oil])//油夠了
+
+			// 如果現在油量能支撐你跑到下個點，錢又花得比剛剛少，就取代，再把狀態拿到pq嘗試
+			if (dp[cur.fr][cur.oil] < dp[to][cur.oil - road_len])
 			{
-				dp[too][top_oil - d] = dp[top_point][top_oil];//直接跑
-				que.push((node){ too , top_oil - d , dp[too][top_oil - d]});
+				dp[to][cur.oil - road_len] = dp[cur.fr][cur.oil];
+				pq.push({to, cur.oil - road_len, dp[to][cur.oil - road_len]});
 			}
 		}
 
-		if (top_oil < oil_size && dp[top_point][top_oil+1] > dp[top_point][top_oil] + oil_price[top_point])//加油
+		//油箱容量沒爆，加看看一滴油，再把狀態推進pq裡
+		if (cur.oil < c && dp[cur.fr][cur.oil + 1] > dp[cur.fr][cur.oil] + p[cur.fr])
 		{
-			dp[top_point][top_oil + 1] = dp[top_point][top_oil] + oil_price[top_point];//+1公升若便宜
-			que.push((node){top_point , top_oil + 1, dp[top_point][top_oil+1]});//敬請排隊
+			dp[cur.fr][cur.oil + 1] = dp[cur.fr][cur.oil] + p[cur.fr];
+			pq.push({cur.fr, cur.oil + 1, dp[cur.fr][cur.oil + 1]});
 		}
+
+		// debug();
 	}
 
-
+	return dp[e][0];
 }
-
-
 
 int main()
 {
-
-	#ifdef DBG
+#ifdef DBG
 	freopen(PROBLEM TESTC ".in", "r", stdin);
 	freopen(PROBLEM ".out", "w", stdout);
-	#endif
+#endif
 
-	while(cin >> city >> road)
+	while (~scanf("%d %d", &city, &road))
 	{
-		for (int i = 0; i < city; ++i)
+		memset(p, 0, sizeof(p));
+		for (int i = 0; i < MAXN; i++)
+			G[i].clear();
+
+		for (int i = 0; i < city; i++)
+			scanf("%d", &p[i]);
+
+		for (int i = 0, a, b, c; i < road; i++)
 		{
-			cin >> oil_price[i];
+			scanf("%d %d %d", &a, &b, &c);
+			add_edge(a, b, c);
 		}
-		int begin,end,dis;
-		for (int i = 0; i < road; ++i)
+
+		scanf("%d", &q);
+
+		for (int i = 0; i < q; i++)
 		{
-			cin >> begin >> end >> dis;
-			Addedge(begin,end,dis);
-		}
-		
-
-
-		int question;
-		int oil_size;
-		int question_start,question_end;
-		cin >> question;
-
-		while(question--)
-		{
-			cin >> oil_size >> question_start >> question_end;
-			run_dijkstra(oil_size,question_start,question_end);
-			// for (int i = 0; i < question_end; ++i)
-			// {
-			// 	for (int j = 0; j < ; ++j)
-			// 	{
-			// 		cout << dp[i][j] << " ";
-			// 	}
-			// 	cout << '\n';
-			// }
-			if (dp[question_end][0] == INF)
-			{
+			scanf("%d %d %d", &c, &s, &e);
+			int ans = dijk(s, e, c);
+			if (ans == INT_MAX)
 				printf("impossible\n");
-			}
 			else
-			{
-				printf("%d\n",dp[question_end][0]);
-			}
+				printf("%d\n", ans);
 		}
-
-
 	}
 
 	return 0;
